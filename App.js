a1a274f1088ebff710e5babec70b2e7850834eac
daazhi <932882600@qq.com>
2018-12-23 15:11:20 +0800

    init files

---
+++ App.js
@@ -0,0 +1,5 @@
+
+import {createAppContainer} from "react-navigation";
+import AppNavigator from "./src/view/Pages";
+
+export default createAppContainer(AppNavigator);
---
+++ index.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+/**
+ * 启动命令:
+ * react-native run-android
+ *
+ * react-navigation:
+ * https://reactnavigation.org/docs/zh-Hans/getting-started.html
+ *
+ * ant-design:
+ * https://rn.mobile.ant.design/components/button-cn/
+ */
